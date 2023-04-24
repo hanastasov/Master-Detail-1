3dc4483ac5227c9491a009f3b7854277fe802654
Katrin <kdragieva@infragistics.com>
2023-04-24 14:37:26 +0300

    Delete src directory

--- src/app/input-details/input-details.component.ts
+++
@@ -1,9 +0,0 @@
-import { Component } from '@angular/core';
-
-@Component({
-  selector: 'app-input-details',
-  templateUrl: './input-details.component.html',
-  styleUrls: ['./input-details.component.scss']
-})
-export class InputDetailsComponent {
-}
