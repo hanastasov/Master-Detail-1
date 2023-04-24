fe355996715a1b6d5287c5c699b345ca0db7d5be
katherinedragieva <kdragieva@infragistics.com>
2023-04-24 22:01:10 +0300

    finish input details view

--- Master-Detail_Angular/src/app/input-details/customer-details/customer-details.component.ts
+++ Master-Detail_Angular/src/app/input-details/customer-details/customer-details.component.ts
@@ -1,9 +1,38 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { IRowSelectionEventArgs } from '@infragistics/igniteui-angular';
+import { Employees, NorthwindService } from 'src/app/services/northwind.service';
 
 @Component({
   selector: 'app-customer-details',
   templateUrl: './customer-details.component.html',
   styleUrls: ['./customer-details.component.scss']
 })
-export class CustomerDetailsComponent {
+export class CustomerDetailsComponent implements OnInit {
+  public selectedEmployee: Employees[] | null = null;
+  public northwindOrders: any = null;
+  public northwindOrderDetails: any = null;
+  public selectedOrdersData: any = [];
+  public selectedOrdersDetails: any = [];
+  public detailsAreLoading = true;
+
+  constructor(private route: ActivatedRoute, private northwindService: NorthwindService) { }
+
+  ngOnInit(): void {
+    this.northwindService.getData('Employees').subscribe(data => this.selectedEmployee = data.filter(employees => {
+      return employees.employeeID == this.route.snapshot.queryParamMap.get('employeeID');
+    }));
+    this.northwindService.getData('Orders').subscribe(data => {
+      this.northwindOrders = data.filter(orders => {
+        return orders.employeeID == this.route.snapshot.queryParamMap.get('employeeID');
+      })
+    });
+    this.northwindService.getData('order_details').subscribe(data => this.northwindOrderDetails = data);
+  }
+
+  public orderSelected(orderID: IRowSelectionEventArgs) {
+    this.detailsAreLoading = true;
+    this.selectedOrdersDetails = this.northwindOrderDetails.filter(el => el.orderID === orderID.newSelection[0]);
+    this.detailsAreLoading = false;
+  }
 }
--- Master-Detail_Angular/src/app/input-details/search-results/search-results.component.ts
+++ Master-Detail_Angular/src/app/input-details/search-results/search-results.component.ts
@@ -1,6 +1,5 @@
 import { Component, OnInit } from '@angular/core';
 import { Employees, NorthwindService } from '../../services/northwind.service';
-import { Subject } from 'rxjs';
 
 @Component({
   selector: 'app-search-results',
@@ -9,7 +8,6 @@ import { Subject } from 'rxjs';
 })
 export class SearchResultsComponent implements OnInit {
   public northwindEmployees: Employees[] | null = null;
-  subject: Subject<Object> = new Subject<Object>();
 
   constructor(private northwindService: NorthwindService) {}
 
@@ -21,7 +19,9 @@ export class SearchResultsComponent implements OnInit {
     this.northwindService.getData('Employees')
     .subscribe(
       response => {
-        this.northwindEmployees = response.filter(el => el.lastName.toLowerCase()  === e.target.value.toLowerCase());
+        this.northwindEmployees = response.filter(employee => {
+          return employee.lastName.toLowerCase().includes(e.target.value);
+        });
       },
       errorResponse => {
         alert("oh no, there was an error when calling the API");
