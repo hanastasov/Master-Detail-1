13ee88db70c2ba070cfd887f03095a229a46371b
Svetloslav Novoselski <svetli.novoselski@gmail.com>
2023-05-02 15:19:08 +0300

    fix grids and improve styling list details page

--- Master details_blazor-wasm/Master details/wwwroot/Script.js
+++ Master details_blazor-wasm/Master details/wwwroot/Script.js
@@ -3,10 +3,13 @@ window.rowSelectionChangingHandler = (args) => {
 }
 
 window.rowSelectionListDetailsChangingHandler = (args) => {
-    console.log(args)
     DotNet.invokeMethodAsync('Master details', 'UpdateSelectedRowFunc', args.detail.added[0].OrderID);
 }
 
+window.rowSelectionChangingHandlerCombo = (args) => {
+    DotNet.invokeMethodAsync('Master details', 'UpdateSelectedRowCombo', args.detail.added[0].OrderID);
+}
 
 igRegisterScript("rowSelectionChangingHandler", rowSelectionChangingHandler, false);
 igRegisterScript("rowSelectionListDetailsChangingHandler", rowSelectionListDetailsChangingHandler, false);
+igRegisterScript("rowSelectionChangingHandlerCombo", rowSelectionChangingHandlerCombo, false);
