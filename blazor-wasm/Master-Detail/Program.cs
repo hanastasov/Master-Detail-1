7a1fe599a99f802b6407bf48283746ae91bbb5b0
Milko Venkov <MVenkov@infragistics.com>
2023-08-22 12:30:20 +0300

    feat(blazor): add Blazor Server master detail sample

--- blazor-wasm/Master-Detail/Program.cs
+++ blazor-wasm/Master-Detail/Program.cs
@@ -1,8 +1,8 @@
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using Master_Detail;
-using IgniteUI.Blazor.Controls;
 using Master_Detail.Northwind;
+using IgniteUI.Blazor.Controls;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
