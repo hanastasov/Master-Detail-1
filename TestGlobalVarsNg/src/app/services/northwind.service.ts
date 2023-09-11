8e1270a7b6ce71fc7e8c725594ff4ce2ef896570
jackofdiamond5 <jackofdiamond596@gmail.com>
2023-09-11 16:22:30 +0300

    chore(ng): clean up

--- TestGlobalVarsNg/src/app/services/northwind.service.ts
+++ TestGlobalVarsNg/src/app/services/northwind.service.ts
@@ -2,8 +2,6 @@ import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable, of, take } from 'rxjs';
 import { Customer } from '../models/customer';
-import { Order } from '../models/order';
-import { Northwind } from './northwind';
 import { CustomerService } from './customer.service';
 
 const API_ENDPOINT = 'https://demodata.grapecity.com/northwind/api/v1';
@@ -18,19 +16,11 @@ export class NorthwindService {
   
   public customer: Customer | undefined;
 
-  public getData(tableName: string): Observable<any> {
-    return of(Northwind[tableName]);
-  }
-
   public getCustomer(id: string) {
     return this.http.get<Customer>(`${API_ENDPOINT}/Customers/${id}`);
   }
 
   public getCustomers(): Observable<Customer[]> {
     return this.http.get<Customer[]>(`${API_ENDPOINT}/Customers`);
-  }
-
-  public getCustomerOrders(customerId: string): Observable<Order[]> {
-    return this.http.get<Order[]>(`${API_ENDPOINT}/Customers/${customerId}/Orders`);
   }
 }
